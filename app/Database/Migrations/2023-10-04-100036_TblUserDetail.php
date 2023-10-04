a109f5dd27d48d423829c7d119b4df99dae0da90
galihsu <indosmartdg@gmail.com>
2023-10-04 17:01:45 +0700

    migrate tbl_user

---
+++ app/Database/Migrations/2023-10-04-100036_TblUserDetail.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TblUserDetail extends Migration
+{
+    public function up()
+    {
+        $fields = [
+            'id_user_detail' => [
+                'type' => 'SMALLINT',
+                // 'constraint' => 5,
+                'auto_increment' => true
+            ],
+            'nik' => [
+                'type' => 'varchar',
+                'constraint' => '20',
+                'null' => true,
+            ],
+            'nm_user' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255'
+            ],
+            'tgl_lahir' => [
+                'type' => 'DATE',
+                'null' => true,
+            ],
+            'tmp_lahir' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type'    => 'TIMESTAMP',
+                'null' => true,
+                // 'default' => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+                // 'default' => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+            'jns_kelamin' => [
+                'type' => 'varchar',
+                'constraint' => '50',
+                'null' => true,
+            ],
+            'img_user' => [
+                'type' => 'varchar',
+                'constraint' => '255',
+                'null' => true,
+            ]
+        ];
+
+        $this->forge->addField($fields);
+        $this->forge->addPrimaryKey('id_user_detail');
+        $attributes = ['ENGINE' => 'InnoDB'];
+        $this->forge->createTable('tbl_user_detail', false, $attributes);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tbl_user_detail');
+    }
+}
