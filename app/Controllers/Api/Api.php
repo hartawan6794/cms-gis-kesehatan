4ac4f71c4f67e60c93b681a16193c47b6138643c
galihsu <indosmartdg@gmail.com>
2023-10-18 15:05:36 +0700

    beberapa perbaikan

---
+++ app/Controllers/Api/Api.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controllers\Api;
+
+use App\Controllers\BaseController;
+use App\Models\TbluserModel;
+use App\Models\UserModel;
+use Config\Services;
+use Firebase\JWT\JWT;
+
+class Api extends BaseController
+{
+    protected $privateKey;
+    protected $db;
+
+    function __construct()
+    {
+		$this->db = db_connect();
+        $this->privateKey = Services::privateKey()['keyServer'];
+    }
+
+    public function tempatKesehatanTerkini(){
+
+        $sql = "SELECT * FROM (SELECT * FROM tbl_klinik                                         
+		UNION ALL                                                                 
+		SELECT * FROM tbl_puskesmas                                               
+		UNION ALL                                                                 
+		SELECT * FROM tbl_rumah_sakit                                             
+		UNION ALL                                                                 
+		SELECT * FROM tbl_rumah_sakit_ibu_anak) z order by created_at DESC LIMIT 5";
+		$data = $this->db->query($sql)->getResult();
+
+        if ($data) {
+            $response = [
+                'status'    => true,
+                'message'   => 'Berhasil Mendapatkan Data',
+                'result'      => $data,
+            ];
+        } else {
+            $response = [
+                'status'    => false,
+                'message'   => 'Gagal Mendapatkan Data',
+                'result'      => []
+            ];
+        }
+
+        return $this->response->setJSON($response);
+
+
+
+
+    }
+
+}
