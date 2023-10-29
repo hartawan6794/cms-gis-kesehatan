f8630e0b656e3da51ec274e6290a5f3ebd45107b
hartawan6794 <julihartawan06@gmail.com>
2023-10-29 21:39:02 +0700

    beberapa perbaikan

--- app/Models/RsiaModel.php
+++ app/Models/RsiaModel.php
@@ -7,10 +7,10 @@ use CodeIgniter\Model;
 class RsiaModel extends Model {
     
 	protected $table = 'tbl_rumah_sakit_ibu_anak';
-	protected $primaryKey = 'id_rsia';
+	protected $primaryKey = 'id';
 	protected $returnType = 'object';
 	protected $useSoftDeletes = false;
-	protected $allowedFields = ['nama_rsia', 'kecamatan', 'deskripsi', 'Latitude', 'longitude', 'gambar', 'is_jadwal', 'created_at', 'updated_at','notelp'];
+	protected $allowedFields = ['nama', 'kecamatan', 'deskripsi', 'Latitude', 'longitude', 'gambar', 'is_jadwal', 'created_at', 'updated_at','notelp'];
 	protected $useTimestamps = false;
 	protected $createdField  = 'created_at';
 	protected $updatedField  = 'updated_at';
